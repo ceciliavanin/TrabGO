b5801e2349dcc476765a0823ab2018112805a733
Cecilia Vanin <ceciliaavanin@gmail.com>
2016-04-24 22:39:07 -0300

    Alterações na classe de montagem das SQL's.

--- src/br/univel/trabalho/Montando.java
+++ src/br/univel/trabalho/Montando.java
@@ -1,5 +1,44 @@
 package br.univel.trabalho;
 
-public class Montando {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.univel.enums.EstadoCivil;
+import br.univel.annotations.*;
+
+public class Montando extends SQLGen {
+		
+		public Montando () {
+			String strCreateTable = getCreateTable(Cliente.class);
+			System.out.println(strCreateTable);
+			
+			String strDropTable = getDropTable(Cliente.class);
+			System.out.println(strDropTable);
+			
+			
+			Cliente cliente = new Cliente(1, "Cecilia", "Rua Souza Naves", "45888888888", EstadoCivil.Solteiro);
+			Connection con = null;
+
+			try {
+
+				con = new Conex();
+
+				PreparedStatement ps = getSqlInsert(con, cliente);
+				ps.executeUpdate();
+				ps = getSqlSelectAll(con, cliente);
+				ps = getSqlSelectById(con, cliente);
+
+				ps.close();
+				con.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+
+			}
+			
+			
+		}
+
 
 }
