4e0f35166688982c9364c43431ddb6e036e1fa56
Cecilia Vanin <ceciliaavanin@gmail.com>
2016-04-24 21:56:34 -0300

    Criação e configuração das classes Conex e ImpDao.

---
+++ src/br/univel/trabalho/ImpDao.java
@@ -0,0 +1,37 @@
+package br.univel.trabalho;
+
+import java.util.List;
+
+public class ImpDao implements Dao {
+
+	@Override
+	public void salvar(Object t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object buscar(Object k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizar(Object t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir(Object k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List listarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
